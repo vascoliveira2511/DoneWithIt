cf5ede8d50626e8cee4682b17d4df1bbd1864048
vascoliveira2511 <vascoliveira2511@gmail.com>
2025-03-31 20:32:44 +0100

    Welcome Screen and View Screen

--- App.js
+++ App.js
@@ -1,26 +1,8 @@
-import { View } from "react-native-web";
+import React from "react";
+import { View } from "react-native";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
 
 export default function App() {
-  return (
-    <View style={{ backgroundColor: "#fff", flex: 1 }}>
-      <View
-        style={{
-          backgroundColor: "dodgerblue",
-          flex: 2,
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "gold",
-          flex: 1,
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "tomato",
-          flex: 1,
-        }}
-      />
-    </View>
-  );
+  return <ViewImageScreen />;
 }
